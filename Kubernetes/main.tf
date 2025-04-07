1d55598b0f05cb95b6876607263b31fae3e0a01a
João Victor <jvictor_oliveira@outlook.com.br>
2025-04-07 12:58:12 +0100

    Iniciating with Datasources and Outputs

--- Kubernetes/main.tf
+++ Kubernetes/main.tf
@@ -1,8 +1,17 @@
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "rocketseat-bucket-iac"
+  bucket = "rocketseat-bucket-iac-${terraform.workspace}373563"
 
-    tags = {
-      Name = "First-bucket"
-      Iac = true
-    }
+  tags = {
+    Name        = "First-bucket"
+    Iac         = true
+    Owner       = "Victor"
+    Environment = "Development"
+    CreatedAt   = "2023-10-01"
+    CreatedBy   = "Terraform"
+    Version     = "1.0"
+    Description = "This is my first bucket created with Terraform"
+    Project     = "Terraform"
+    Team        = "DevOps"
+    Context     = "${terraform.workspace}"
+  }
 }
--- Kubernetes/providers.tf
+++ Kubernetes/providers.tf
@@ -1,13 +1,15 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.94.0"
+      source  = "hashicorp/aws"
+      version = "5.36.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "Victor" 
+  # Configure the AWS Provider
+  region = "us-east-1"
+  profile = "victor"
+  
 }
