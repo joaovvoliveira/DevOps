425248868dd61a7a5001cbeca3d68e7260629a45
João Victor <jvictor_oliveira@outlook.com.br>
2025-04-08 11:26:02 +0100

    my first ecs2 Instance made with terraform

--- Terraform/TerraformClasses/main.tf
+++ Terraform/TerraformClasses/main.tf
@@ -9,9 +9,24 @@ resource "aws_s3_bucket" "s3_bucket" {
     CreatedAt   = "2023-10-01"
     CreatedBy   = "Terraform"
     Version     = "1.0"
-    Description = "This is my first bucket created with Terraform"
+    Context     = "${terraform.workspace}"
+  }
+}
+
+module "ec2_instance" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 5.0"
+  name                   = "${terraform.workspace}-instance"
+  instance_type          = "t2.micro"
+  key_name               = "victor-key"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0169c99bf4d715390"]
+  subnet_id              = "subnet-0b03e6dcbce443d0d"
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Description = "This is my first ec2 created with Terraform"
     Project     = "Terraform"
     Team        = "DevOps"
-    Context     = "${terraform.workspace}"
   }
 }
